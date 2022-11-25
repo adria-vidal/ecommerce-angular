c4173a7a7255e41e3385ecb0de9b9442b3371be8
Adria Vidal <adriavidal1998@gmail.com>
2022-11-25 23:27:06 +0100

    maquetado login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,16 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { NavComponent } from './elements/nav/nav.component';
+import { LoginComponent } from './elements/login/login.component';
 
 const routes: Routes = [
   {
     path: 'nav',
     component: NavComponent,
+  },
+  {
+    path: 'login',
+    component: LoginComponent,
   },
 ];
 
