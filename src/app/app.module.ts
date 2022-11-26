d316e9f64da02b7ed62ee49910dc55c454755651
Adria Vidal <adriavidal1998@gmail.com>
2022-11-26 18:58:34 +0100

    maquetación de login y register, enrutamiento y colocación del footer ok

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 import { ElementsModule } from './elements/elements.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import { ElementsModule } from './elements/elements.module';
   imports: [
     BrowserModule,
     ElementsModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
