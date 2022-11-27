709826d070a1acebae0b050c27794295b1acebf7
carlesnacher <carlesnacher@gmail.com>
2022-11-27 23:23:01 +0100

    new component articulos

--- src/app/elements/elements.module.ts
+++ src/app/elements/elements.module.ts
@@ -8,9 +8,10 @@ import { TableUsersComponent } from './table-users/table-users.component';
 import { CategoriesComponent } from './categories/categories.component';
 import { PerfilComponent } from './perfil/perfil.component';
 import { NewsletterComponent } from './newsletter/newsletter.component';
+import { ArticulosComponent } from './articulos/articulos.component';
 
 @NgModule({
-  declarations: [NavComponent, LoginComponent, FooterComponent, RegisterComponent, TableUsersComponent, CategoriesComponent, PerfilComponent, NewsletterComponent],
+  declarations: [NavComponent, LoginComponent, FooterComponent, RegisterComponent, TableUsersComponent, CategoriesComponent, PerfilComponent, NewsletterComponent, ArticulosComponent],
   imports: [CommonModule],
   exports: [NavComponent,LoginComponent,FooterComponent,RegisterComponent,PerfilComponent]
 })
