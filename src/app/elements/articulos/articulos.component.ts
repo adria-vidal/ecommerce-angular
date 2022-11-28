e8779a250f40f0ab8b28ee483e2aaa732e122adf
Adria Vidal <adriavidal1998@gmail.com>
2022-11-28 12:42:15 +0100

    acceso a datos api

--- src/app/elements/articulos/articulos.component.ts
+++ src/app/elements/articulos/articulos.component.ts
@@ -7,6 +7,8 @@ import { ConsultasapiService } from 'src/app/services/consultasapi.service';
   styleUrls: ['./articulos.component.css'],
 })
 export class ArticulosComponent implements OnInit {
+
+
   articulos: any;
 
   constructor(private consulta: ConsultasapiService) {}
@@ -14,7 +16,7 @@ export class ArticulosComponent implements OnInit {
   ngOnInit() {
     this.consulta.retornar().subscribe((result) => {
       console.log(result);
-      result = this.articulos;
+      this.articulos = result;
     });
   }
 }
