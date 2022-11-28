055d587163f13d7d914575844736e876a5e27b6d
Adria Vidal <adriavidal1998@gmail.com>
2022-11-28 13:16:50 +0100

    categorias con datos de la api terminado

--- src/app/elements/categories/categories.component.ts
+++ src/app/elements/categories/categories.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ConsultasapiService } from 'src/app/services/consultasapi.service';
 
 @Component({
   selector: 'app-categories',
   templateUrl: './categories.component.html',
-  styleUrls: ['./categories.component.css']
+  styleUrls: ['./categories.component.css'],
 })
-export class CategoriesComponent {
+export class CategoriesComponent implements OnInit {
+  constructor(private consulta: ConsultasapiService) {}
 
+  categories: any;
+
+  ngOnInit() {
+    this.consulta.getCategory().subscribe((result) => {
+      console.log(result);
+      this.categories = result;
+    });
+  }
 }
