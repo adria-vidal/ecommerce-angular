6a02097c8cd681707370506df2a0027e7a41472c
Adria Vidal <adriavidal1998@gmail.com>
2022-11-30 09:41:50 +0100

    logout

--- src/app/elements/perfil/perfil.component.ts
+++ src/app/elements/perfil/perfil.component.ts
@@ -1,11 +1,21 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { UserService } from 'src/app/services/user.service';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-perfil',
   templateUrl: './perfil.component.html',
-  styleUrls: ['./perfil.component.css']
+  styleUrls: ['./perfil.component.css'],
 })
-export class PerfilComponent {
-  
+export class PerfilComponent implements OnInit {
+  constructor(private userService: UserService, private router: Router) {}
 
+  ngOnInit(): void {}
+  onClick() {
+    this.userService
+      .logout()
+      .then((response) => {
+        this.router.navigate(['/login']);
+      })
+      .catch((error) => console.log(error));
+  }
 }
